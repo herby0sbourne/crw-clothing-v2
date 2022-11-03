eb91e3b6379ad33b5278084b71e466801d869c52
Herby <herbyosbourne31@gmail.com>
2022-11-03 10:45:08 -0500

    create Authentication route/page component

---
+++ src/routes/authentication/Authentication.jsx
@@ -0,0 +1,15 @@
+import SignUpForm from '../../components/sign-up-form/SignUpForm';
+import SignInForm from '../../components/sign-in-form/SignInForm';
+
+import './authentication.scss';
+
+const Authentication = () => {
+    return (
+        <div className="authentication-container">
+            <SignInForm />
+            <SignUpForm />
+        </div>
+    );
+};
+
+export default Authentication;
