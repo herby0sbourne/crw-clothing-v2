9c45a8256dd9057e00928fd8e089563ef809399d
Herby <herbyosbourne31@gmail.com>
2022-11-03 10:47:08 -0500

    export signUserInWithEmailAndPassword function

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,20 +1,21 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 import {
-  getAuth,
-  signInWithRedirect,
-  signInWithPopup,
-  createUserWithEmailAndPassword,
-  GoogleAuthProvider,
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBBuyKADtiUDgjue6uD7uGgJFxizomdlxQ',
-  authDomain: 'crwn-store-v2.firebaseapp.com',
-  projectId: 'crwn-store-v2',
-  storageBucket: 'crwn-store-v2.appspot.com',
-  messagingSenderId: '969270162050',
-  appId: '1:969270162050:web:7f232e9bbc5bcb56a7ba9b',
+    apiKey: 'AIzaSyBBuyKADtiUDgjue6uD7uGgJFxizomdlxQ',
+    authDomain: 'crwn-store-v2.firebaseapp.com',
+    projectId: 'crwn-store-v2',
+    storageBucket: 'crwn-store-v2.appspot.com',
+    messagingSenderId: '969270162050',
+    appId: '1:969270162050:web:7f232e9bbc5bcb56a7ba9b',
 };
 
 const app = initializeApp(firebaseConfig);
@@ -30,26 +31,32 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInfo = {}) => {
-  if (!userAuth) return;
+    if (!userAuth) return;
 
-  const userDocRef = doc(db, 'users', userAuth.uid);
+    const userDocRef = doc(db, 'users', userAuth.uid);
 
-  const userSnapshot = await getDoc(userDocRef);
-  if (!userSnapshot.exists()) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
+    const userSnapshot = await getDoc(userDocRef);
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
 
-    try {
-      await setDoc(userDocRef, { displayName, email, createdAt, ...additionalInfo });
-    } catch (error) {
-      console.log('error creating user', error.message);
+        try {
+            await setDoc(userDocRef, { displayName, email, createdAt, ...additionalInfo });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
     }
-  }
 
-  return userDocRef;
+    return userDocRef;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
-  if (!email || !password) return;
-  return await createUserWithEmailAndPassword(auth, email, password);
+    if (!email || !password) return;
+    return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signUserInWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
 };
