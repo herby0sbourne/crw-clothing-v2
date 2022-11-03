859bb6902b3d94917dad6a29354b0e622a7d8b0a
Herby <herbyosbourne31@gmail.com>
2022-11-03 10:54:49 -0500

    refactor to use id and update method

--- src/utils/notification.utils.js
+++ src/utils/notification.utils.js
@@ -1,16 +1,45 @@
 import { toast } from 'react-toastify';
 
-const showToastMessage = (type, message) => {
-  toast[type](`${message}`, {
-    position: 'top-center',
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: 'light',
-  });
+const showToastMessage = (type, message, id = null) => {
+    // toast.update(id, { render: 'Creating user failed', type: 'error', isLoading: false });
+    if (id) {
+        toast.update(id, {
+            render: `${message}`,
+            type: `${type}`,
+            isLoading: false,
+            position: 'top-center',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'light',
+        });
+        return;
+    }
+
+    return toast[type](`${message}`, {
+        position: 'top-center',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+    });
+
+    // toast[type](`${message}`, {
+    //   position: 'top-center',
+    //   autoClose: 5000,
+    //   hideProgressBar: false,
+    //   closeOnClick: true,
+    //   pauseOnHover: true,
+    //   draggable: true,
+    //   progress: undefined,
+    //   theme: 'light',
+    // });
 };
 
 export default showToastMessage;
