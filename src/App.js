c69d74e8c5bdcd97073224c33d08e301489d903e
Herby <herbyosbourne31@gmail.com>
2022-11-03 10:51:17 -0500

    prettier format

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
+import Authentication from './routes/authentication/Authentication';
 import Home from './routes/home/Home';
 import Navigation from './routes/navigation/Navigation';
-import SignIn from './routes/signIn/SignIn';
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="signIn" element={<SignIn />} />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Navigation />}>
+                <Route index element={<Home />} />
+                <Route path="auth" element={<Authentication />} />
+            </Route>
+        </Routes>
+    );
 };
 
 export default App;
--- src/routes/navigation/Navigation.jsx
+++ src/routes/navigation/Navigation.jsx
@@ -4,24 +4,24 @@ import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import './navigation.scss';
 
 const Navigation = () => {
-  return (
-    <>
-      <div className="navigation">
-        <Link to={'/'} className="logo-container">
-          <CrownLogo className="logo" />
-        </Link>
-        <div className="nav-links-container">
-          <Link to={'/shop'} className="nav-link">
-            SHOP
-          </Link>
-          <Link to={'/signIn'} className="nav-link">
-            SIGN IN
-          </Link>
-        </div>
-      </div>
-      <Outlet />
-    </>
-  );
+    return (
+        <>
+            <div className="navigation">
+                <Link to={'/'} className="logo-container">
+                    <CrownLogo className="logo" />
+                </Link>
+                <div className="nav-links-container">
+                    <Link to={'/shop'} className="nav-link">
+                        SHOP
+                    </Link>
+                    <Link to={'/auth'} className="nav-link">
+                        SIGN IN
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </>
+    );
 };
 
 export default Navigation;
