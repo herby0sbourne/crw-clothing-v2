a1d7c2ce9802804c1ccdf06e26da6e84129767cd
Herby <herbyosbourne31@gmail.com>
2022-11-03 00:16:21 -0500

    import notification function

--- src/components/sign-up-form/SignUpForm.jsx
+++ src/components/sign-up-form/SignUpForm.jsx
@@ -1,11 +1,12 @@
-import {useState} from 'react';
-import {ToastContainer, toast} from 'react-toastify';
-import {createAuthUserWithEmailAndPassword, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import FormInput from '../formInput/FormInput';
+import CustomBtn from '../customBtn/CustomBtn';
+import showToastMessage from '../../utils/notification.utils';
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 import './signupform.scss';
 import 'react-toastify/dist/ReactToastify.css';
-import CustomBtn from '../customBtn/CustomBtn';
 
 const defaultFormFields = {
   displayName: '',
@@ -16,41 +17,41 @@ const defaultFormFields = {
 
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
-  const {displayName, email, password, confirmPassword} = formFields;
+  const { displayName, email, password, confirmPassword } = formFields;
 
   const handleChange = (e) => {
-    const {name, value} = e.target;
-    setFormFields({...formFields, [name]: value});
+    const { name, value } = e.target;
+    setFormFields({ ...formFields, [name]: value });
   };
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
-  const showToastMessage = () => {
-    toast.error('password does not match', {
-      position: 'top-center',
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: 'light',
-    });
-  };
+  // const showToastMessage = () => {
+  //   toast.error('password does not match', {
+  //     position: 'top-center',
+  //     autoClose: 5000,
+  //     hideProgressBar: false,
+  //     closeOnClick: true,
+  //     pauseOnHover: true,
+  //     draggable: true,
+  //     progress: undefined,
+  //     theme: 'light',
+  //   });
+  // };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (password !== confirmPassword) {
-      showToastMessage();
+      showToastMessage('error', 'password does not match');
       return;
     }
     // const id = toast.loading('Please wait...');
 
     try {
-      const {user} = await createAuthUserWithEmailAndPassword(email, password);
-      await createUserDocumentFromAuth(user, {displayName});
+      const { user } = await createAuthUserWithEmailAndPassword(email, password);
+      await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
       // toast.update(id, { render: 'User Created', type: 'success', isLoading: false });
     } catch (err) {
@@ -66,40 +67,40 @@ const SignUpForm = () => {
   };
 
   return (
-      <div className={'sign-up-container'}>
-        <h2>Dont have an account</h2>
-        <span>Sign up width your email and password</span>
-        <form onSubmit={handleSubmit}>
-          <FormInput
-              label="Display Name"
-              type="text"
-              value={displayName}
-              name="displayName"
-              onChange={handleChange}
-              required
-          />
-          <FormInput label="Email" type="email" value={email} name="email" onChange={handleChange} required/>
-          <FormInput
-              label={'Password'}
-              type="password"
-              value={password}
-              name="password"
-              onChange={handleChange}
-              required
-          />
-          <FormInput
-              label={'Confirm Password'}
-              type="password"
-              value={confirmPassword}
-              name="confirmPassword"
-              onChange={handleChange}
-              required
-          />
+    <div className={'sign-up-container'}>
+      <h2>Dont have an account</h2>
+      <span>Sign up width your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Display Name"
+          type="text"
+          value={displayName}
+          name="displayName"
+          onChange={handleChange}
+          required
+        />
+        <FormInput label="Email" type="email" value={email} name="email" onChange={handleChange} required />
+        <FormInput
+          label={'Password'}
+          type="password"
+          value={password}
+          name="password"
+          onChange={handleChange}
+          required
+        />
+        <FormInput
+          label={'Confirm Password'}
+          type="password"
+          value={confirmPassword}
+          name="confirmPassword"
+          onChange={handleChange}
+          required
+        />
 
-          <CustomBtn type={'submit'}>Sign Up</CustomBtn>
-        </form>
-        <ToastContainer newestOnTop={false} rtl={false} pauseOnFocusLoss/>
-      </div>
+        <CustomBtn type={'submit'}>Sign Up</CustomBtn>
+      </form>
+      <ToastContainer newestOnTop={false} rtl={false} pauseOnFocusLoss />
+    </div>
   );
 };
 
