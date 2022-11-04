54725aabf9bc0ece2c90d1c6149cd57482b6053b
Herby <herbyosbourne31@gmail.com>
2022-11-04 14:48:46 -0500

    prettier format

--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -1,14 +1,15 @@
 import CategoryItem from '../categoryItem/CategoryItem';
 import './directory.scss';
 
-const Directory = ({ categories }) => {
-  return (
-    <div className="directory-container">
-      {categories.map((category) => {
-        return <CategoryItem key={category.id} {...category} />;
-      })}
-    </div>
-  );
+const Directory = ({categories}) => {
+
+    return (
+        <div className="directory-container">
+            {categories.map((category) => {
+                return <CategoryItem key={category.id} {...category} />;
+            })}
+        </div>
+    );
 };
 
 export default Directory;
