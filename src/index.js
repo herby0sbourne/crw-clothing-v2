ba5b7c720e99af631b8c369bc38bf6ee85999940
Herby <herbyosbourne31@gmail.com>
2022-11-04 13:32:40 -0500

    import ProductsProvider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './context/User.Context';
+import { ProductsProvider } from './context/Products.Context';
 import App from './App';
 
 import './index.scss';
@@ -11,7 +12,9 @@ root.render(
     <React.StrictMode>
         <BrowserRouter>
             <UserProvider>
-                <App />
+                <ProductsProvider>
+                    <App />
+                </ProductsProvider>
             </UserProvider>
         </BrowserRouter>
     </React.StrictMode>
