812e6ceb89a6f2d4bc70558044358fcecd14006a
Herby <herbyosbourne31@gmail.com>
2022-11-04 16:21:21 -0500

    crate function to update cartItems and update totalItems count

--- src/context/Cart.Context.jsx
+++ src/context/Cart.Context.jsx
@@ -1,14 +1,47 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+
+const addCartItem = (cartItems, productToAdd) => {
+    //* find if productToAdd{} is already in cartItems[]
+    const existingCartItem = cartItems.find((cartItem) => {
+        return cartItem.id === productToAdd.id;
+    });
+
+    //* if found increase quantity by 1
+    if (existingCartItem) {
+        return cartItems.map((cartItem) => {
+            return cartItem.id === productToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem;
+        });
+    }
+
+    //* return new array with modified cartItem or new cart item
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartContext = createContext({
+    cartItems: [],
+    totalItems: 0,
     toggleDropdown: false,
-    setToggleDropdown: () => null,
+    setToggleDropdown: () => {},
+    addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
     const [toggleDropdown, setToggleDropdown] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+    const [totalItems, setTotalItems] = useState(0);
 
-    const value = { toggleDropdown, setToggleDropdown };
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    };
+
+    useEffect(() => {
+        const newTotalItems = cartItems.reduce((total, cartItem) => {
+            return total + cartItem.quantity;
+        }, 0);
+        setTotalItems(newTotalItems);
+    }, [cartItems]);
+
+    const value = { toggleDropdown, cartItems, totalItems, setToggleDropdown, addItemToCart };
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
 
