66681435593fad0055cb1419496f1d831b39df17
Herby <herbyosbourne31@gmail.com>
2022-11-04 16:18:36 -0500

    use carItems from CartContext to display cartItem

--- src/components/cartDropdown/CartDropdown.jsx
+++ src/components/cartDropdown/CartDropdown.jsx
@@ -1,11 +1,19 @@
+import { useContext } from 'react';
+import CartItem from '../cartItem/CartItem';
 import CustomBtn from '../customBtn/CustomBtn';
+import { CartContext } from '../../context/Cart.Context';
 
 import './cart-dropdown.scss';
 
 const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext);
     return (
         <div className={'cart-dropdown-container'}>
-            <div className="cart-item"></div>
+            <div className="cart-items">
+                {cartItems.map((cartItem) => {
+                    return <CartItem key={cartItem.id} cartItem={cartItem} />;
+                })}
+            </div>
             <CustomBtn>GO TO CHECKOUT</CustomBtn>
         </div>
     );
