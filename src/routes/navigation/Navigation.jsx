0237254a86f73cd6eb940f89c8ac9fb7187204f8
Herby <herbyosbourne31@gmail.com>
2022-11-04 11:04:45 -0500

    check if user is login and change value

--- src/routes/navigation/Navigation.jsx
+++ src/routes/navigation/Navigation.jsx
@@ -1,9 +1,19 @@
+import { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import { UserContext } from '../../context/User.Context';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.scss';
 
 const Navigation = () => {
+    const { currentUser } = useContext(UserContext);
+
+    const signOutHandler = async () => {
+        await signOutUser();
+        currentUser(null);
+    };
+
     return (
         <>
             <div className="navigation">
@@ -14,9 +24,15 @@ const Navigation = () => {
                     <Link to={'/shop'} className="nav-link">
                         SHOP
                     </Link>
-                    <Link to={'/auth'} className="nav-link">
-                        SIGN IN
-                    </Link>
+                    {currentUser ? (
+                        <span className="nav-link" onClick={signOutHandler}>
+                            SIGN OUT
+                        </span>
+                    ) : (
+                        <Link to={'/auth'} className="nav-link">
+                            SIGN IN
+                        </Link>
+                    )}
                 </div>
             </div>
             <Outlet />
