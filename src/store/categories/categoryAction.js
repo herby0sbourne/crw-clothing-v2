128abf0f1b7ff2c2392700bf72d10711218d50ae
Herby <herbyosbourne31@gmail.com>
2022-11-08 21:10:56 -0500

    create categories and CategoriesReducer for redux

---
+++ src/store/categories/categoryAction.js
@@ -0,0 +1,4 @@
+import CATEGORIES_ACTION_TYPES from './categoryTypes';
+import createAction from '../../utils/reducer/reducer.utils';
+
+export const setCategories = (categoriesArray) => createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
