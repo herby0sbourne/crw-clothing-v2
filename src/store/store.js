0725006b022001b1ec7634af7bf34976b260fb05
Herby <herbyosbourne31@gmail.com>
2022-11-08 21:17:34 -0500

    create a redux store

---
+++ src/store/store.js
@@ -0,0 +1,23 @@
+import {compose, createStore, applyMiddleware} from 'redux';
+// import logger from 'redux-logger';
+import {rootReducer} from './root-reducer';
+
+const loggerMiddleware = (store) => (next) => (action) => {
+    if (!action.type) {
+        return next(action)
+    }
+
+    console.log('type: ', action.type)
+    console.log('payload: ', action.payload)
+    console.log('currentState: ', store.getState())
+
+    next(action)
+    console.log('next state: ', store.getState())
+}
+
+
+const middleware = [loggerMiddleware];
+// const composedEnhancers = compose(applyMiddleware(...middleware));
+const composedEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(rootReducer, undefined, composedEnhancers(applyMiddleware(...middleware)));
