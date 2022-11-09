919330bb9959cb78895ce4d6efa38bffd11cbb18
Herby <herbyosbourne31@gmail.com>
2022-11-08 21:06:48 -0500

    refactor code to check user status on mount

--- src/App.js
+++ src/App.js
@@ -4,8 +4,26 @@ import Shop from './routes/shop/Shop';
 import Checkout from './routes/checkout/Checkout';
 import Navigation from './routes/navigation/Navigation';
 import Authentication from './routes/authentication/Authentication';
+import { useEffect } from 'react';
+import { createUserDocumentFromAuth, onAuthStateChangeListener } from './utils/firebase/firebase.utils';
+import { setCurrentUser } from './store/user/userAction';
+import { useDispatch } from 'react-redux';
 
 const App = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangeListener((user) => {
+            if (user) {
+                createUserDocumentFromAuth(user);
+            }
+
+            dispatch(setCurrentUser(user));
+        });
+
+        return unsubscribe;
+    }, []);
+
     return (
         <Routes>
             <Route path="/" element={<Navigation />}>
