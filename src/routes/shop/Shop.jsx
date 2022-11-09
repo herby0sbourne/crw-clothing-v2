12326888974a8acab7d51de359ddba9deea383f3
Herby <herbyosbourne31@gmail.com>
2022-11-08 21:18:32 -0500

    refactor code to get store categories on mount

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-import CategoriesPreview from '../../components/categoriesPreview/CategoriesPreview';
 import Category from '../../components/category/Category';
+import { setCategories } from '../../store/categories/categoryAction';
+import { getCollectionAndDocuments } from '../../utils/firebase/firebase.utils';
+import CategoriesPreview from '../../components/categoriesPreview/CategoriesPreview';
+
 import './shop.scss';
 
 const Shop = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        (async () => {
+            const categoriesArray = await getCollectionAndDocuments();
+
+            dispatch(setCategories(categoriesArray));
+        })();
+    }, []);
+
     return (
         <Routes>
             <Route index element={<CategoriesPreview />} />
