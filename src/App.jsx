49829edc252ecfc8a9c6c7cb5a9f9234b68c7e9d
Herby <herbyosbourne31@gmail.com>
2022-11-14 12:54:57 -0500

    import react-toast css

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import Home from './routes/home/Home';
 import Shop from './routes/shop/Shop';
-// import Checkout from './routes/checkout/Checkout';
+import Checkout from './routes/checkout/Checkout';
 import Navigation from './routes/navigation/Navigation';
 import Authentication from './routes/authentication/Authentication';
 import { checkUserSession } from './store/user/userAction';
-import StripeCheckOut from './components/stripeCheckout/StripeCheckout';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
     const dispatch = useDispatch();
@@ -22,7 +23,7 @@ const App = () => {
             <Route path="/" element={<Navigation />}>
                 <Route index element={<Home />} />
                 <Route path="shop/*" element={<Shop />} />
-                <Route path="checkout" element={<StripeCheckOut />} />
+                <Route path="checkout" element={<Checkout />} />
                 <Route path="auth" element={<Authentication />} />
             </Route>
         </Routes>
