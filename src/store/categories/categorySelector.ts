0b7eeefd58bdf7ecefff3c7329cd69169230f14d
Herby <herbyosbourne31@gmail.com>
2022-11-16 18:24:09 -0500

    convert category to typescript

--- src/store/categories/categorySelector.ts
+++ src/store/categories/categorySelector.ts
@@ -1,18 +1,22 @@
 import { createSelector } from 'reselect';
+import { CategoriesState } from './categoryReducer';
+import { CategoryMap } from './categoryTypes';
 
-const selectCategoryReducer = (state) => state.categories;
+const selectCategoryReducer = (state): CategoriesState => state.categories;
 
 export const selectCategories = createSelector(
     [selectCategoryReducer],
     (categoriesSlice) => categoriesSlice.categories
 );
 
-export const selectCategoriesMap = createSelector([selectCategories], (category) =>
-    category.reduce((acc, category) => {
-        const { title, items } = category;
-        acc[title.toLowerCase()] = items;
-        return acc;
-    }, {})
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (category): CategoryMap =>
+        category.reduce((acc, category) => {
+            const { title, items } = category;
+            acc[title.toLowerCase()] = items;
+            return acc;
+        }, {} as CategoryMap)
 );
 
 export const selectCategoriesIsLoading = createSelector(
