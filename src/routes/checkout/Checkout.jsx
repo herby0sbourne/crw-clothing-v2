4bc0ae646cd02e534471c9b6569a7a66ecfc1c5c
Herby <herbyosbourne31@gmail.com>
2022-11-16 14:02:01 -0500

    refactor code wrap paymentform in Stripe Elements

--- src/routes/checkout/Checkout.jsx
+++ src/routes/checkout/Checkout.jsx
@@ -1,14 +1,25 @@
-import { useSelector } from 'react-redux';
-import CheckoutItem from '../../components/checkoutitem/CheckoutItem';
-import PaymentForm from '../../components/paymentForm/PaymentForm';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from '../../stripe/stripe';
+import { setToggleDropdown } from '../../store/cart/cartAction';
 import { selectCartItems, selectCartTotal } from '../../store/cart/cartSelector';
+
+import PaymentForm from '../../components/paymentForm/PaymentForm';
+import CheckoutItem from '../../components/checkoutitem/CheckoutItem';
 import StripeCheckOut from '../../components/stripeCheckout/StripeCheckout';
 
 import './checkout.scss';
 
 const Checkout = () => {
+    const dispatch = useDispatch();
     const totalCost = useSelector(selectCartTotal);
     const cartItems = useSelector(selectCartItems);
+
+    useEffect(() => {
+        dispatch(setToggleDropdown(false));
+        // eslint-disable-next-line
+    }, []);
 
     return (
         <div className="checkout-container">
@@ -33,7 +44,11 @@ const Checkout = () => {
                 return <CheckoutItem key={cartItem.id} cartItem={cartItem} />;
             })}
             <span className="total">Total: ${totalCost}</span>
-            {process.env.REACT_APP_NETLIFY_SERVERLESS_FUNCTION && <PaymentForm />}
+            {process.env.REACT_APP_NETLIFY_SERVERLESS_FUNCTION && (
+                <Elements stripe={stripePromise}>
+                    <PaymentForm />
+                </Elements>
+            )}
             {process.env.REACT_APP_VERCEL_SERVERLESS_FUNCTION && <StripeCheckOut />}
         </div>
     );
