874d4b630d736ff87ba6dbe8b24d80318809f785
Herby <herbyosbourne31@gmail.com>
2022-11-23 18:19:26 -0500

    use Apollo Client for state

--- src/index.js
+++ src/index.js
@@ -4,21 +4,29 @@ import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/Cart.Context';
 import { UserProvider } from './context/User.Context';
 import { CategoriesProvider } from './context/Categories.Context';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import App from './App';
 
 import './index.scss';
 
+const client = new ApolloClient({
+    uri: 'https://crwn-clothing.com/',
+    cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <UserProvider>
-                <CategoriesProvider>
-                    <CartProvider>
-                        <App />
-                    </CartProvider>
-                </CategoriesProvider>
-            </UserProvider>
-        </BrowserRouter>
+        <ApolloProvider client={client}>
+            <BrowserRouter>
+                <UserProvider>
+                    <CategoriesProvider>
+                        <CartProvider>
+                            <App />
+                        </CartProvider>
+                    </CategoriesProvider>
+                </UserProvider>
+            </BrowserRouter>
+        </ApolloProvider>
     </React.StrictMode>
 );
