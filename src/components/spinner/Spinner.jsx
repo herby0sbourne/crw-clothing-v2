a16d504c2568655f8c2334be2aa02515bd69439a
Herby <herbyosbourne31@gmail.com>
2022-11-23 18:17:18 -0500

    create spinner component

---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,11 @@
+import './spinner.scss';
+
+const Spinner = () => {
+    return (
+        <div className="SpinnerOverlay">
+            <div className="SpinnerContainer" />
+        </div>
+    );
+};
+
+export default Spinner;
